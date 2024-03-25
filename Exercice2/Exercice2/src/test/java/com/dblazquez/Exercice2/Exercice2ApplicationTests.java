a43ba38470307fa353200968c646d734c0a29e65
ZenQ98 <dblazquez1998@gmail.com>
2024-03-25 20:02:02 +0100

    Exercice2
    Base Exercice2

---
+++ Exercice2/Exercice2/src/test/java/com/dblazquez/Exercice2/Exercice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dblazquez.Exercice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
