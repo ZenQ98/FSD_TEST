a39b9cdf22abf6a8dafb0a5a5e1288554e72d14f
ZenQ98 <dblazquez1998@gmail.com>
2024-03-26 17:37:48 +0100

    Base Exercice3
    Base Exercice3

--- exercice3/src/app/app.component.spec.ts
+++ exercice3/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'exercice3' title`, () => {
+  it(`should have the 'Exercice3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('exercice3');
+    expect(app.title).toEqual('Exercice3');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, exercice3');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Exercice3');
   });
 });
